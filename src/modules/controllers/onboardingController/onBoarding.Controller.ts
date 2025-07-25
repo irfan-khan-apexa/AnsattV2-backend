28745d90192b75d7a5447f9966fc3e477101ebd0
Irfan Khan <mohdirfankhan1997@gmail.com>
2025-07-25 14:51:39 +0530

    Fix: added passport_photo declaration

--- src/modules/controllers/onboardingController/onBoarding.Controller.ts
+++ src/modules/controllers/onboardingController/onBoarding.Controller.ts
@@ -210,6 +210,7 @@ const createOnboarding = async (
   try {
     const file = req.files as any;
 
+    const passport_photo = file?.passport_photo?.[0]?.location || null;
     const aadhar_photo = file?.aadhar_photo?.[0]?.location || null;
     const pan_photo = file?.pan_photo?.[0]?.location || null;
     const resume = file?.resume?.[0]?.location || null;
