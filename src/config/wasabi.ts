694ea491a1c6536d6ca65429371ec26a6d6d2de6
irfan-khan-apexa <ikhan@apexaglobal.com>
2026-05-22 13:38:51 +0530

    added @aws-sdk/client-s3 dependency 22-05-2026

--- src/config/wasabi.ts
+++ src/config/wasabi.ts
@@ -1,20 +1,20 @@
-// import { S3Client } from "@aws-sdk/client-s3";
-// import dotenv from "dotenv";
-// dotenv.config();
+import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const wasabiS3 = new S3Client({
-//   region: process.env.WASABI_REGION || "ap-northeast-1",
-//   endpoint:
-//     process.env.WASABI_ENDPOINT || "https://s3.ap-northeast-1.wasabisys.com",
-//   credentials: {
-//     accessKeyId: process.env.WASABI_ACCESS_KEY!,
-//     secretAccessKey: process.env.WASABI_SECRET_KEY!,
-//   },
-//   forcePathStyle: true,
+const wasabiS3 = new S3Client({
+  region: process.env.WASABI_REGION || "ap-northeast-1",
+  endpoint:
+    process.env.WASABI_ENDPOINT || "https://s3.ap-northeast-1.wasabisys.com",
+  credentials: {
+    accessKeyId: process.env.WASABI_ACCESS_KEY!,
+    secretAccessKey: process.env.WASABI_SECRET_KEY!,
+  },
+  forcePathStyle: true,
+});
+// console.log("Wasabi Credentials:", {
+//   accessKeyId: process.env.WASABI_ACCESS_KEY,
+//   secretAccessKey: process.env.WASABI_SECRET_KEY,
 // });
-// // console.log("Wasabi Credentials:", {
-// //   accessKeyId: process.env.WASABI_ACCESS_KEY,
-// //   secretAccessKey: process.env.WASABI_SECRET_KEY,
-// // });
 
-// export default wasabiS3;
+export default wasabiS3;
