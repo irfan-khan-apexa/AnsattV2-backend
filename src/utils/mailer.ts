27b72083001b00f5636400fdf744ce98a23250ca
irfan-khan-apexa <ikhan@apexaglobal.com>
2026-05-22 14:18:12 +0530

    Fixed nodemailer

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -2,15 +2,19 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT)|| 587,
-    secure: false, //for gmail 587 port ,remove for mailtrap.io
+  port: Number(process.env.SMTP_PORT) || 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
-export const sendMail = async (to: string | string[], subject: string, html: string) => {
+export const sendMail = async (
+  to: string | string[],
+  subject: string,
+  html: string
+) => {
   await transporter.sendMail({
     from: process.env.SMTP_FROM,
     to,
